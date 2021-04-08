9ecfb25cdfa1a3cf05509251bd31702396a587b7
emmac124 <emma.cooperhawkins@gmail.com>
2021-04-08 12:47:49 -0700

    added material for heroku

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
+const dotenv = require("dotenv").config()
+const express = require("express")
+const app = express()
 const server = require('./api/server');
+const port = process.env.PORT || 1245
+// const path = require("path")
 
-const port = 5000;
+app.use("/api/users/:id", (_,res) => {
+    res.json({data: "users information"})
+})
 
 // START YOUR SERVER HERE
+server.listen(port, () => {
+    console.log(`Running on port ${port}`)
+}) 
